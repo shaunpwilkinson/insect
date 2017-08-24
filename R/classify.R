f5bb22c573068a0f83f9f8126edee74c12c29700
shaunpwilkinson <shaunpwilkinson@gmail.com>
2017-08-24 14:39:59 +0900

    Major changes to 'classify'

--- R/classify.R
+++ R/classify.R
@@ -32,18 +32,12 @@
 #' @examples
 #'   ##TBA
 ################################################################################
-classify <- function(x, tree, threshold = 0.99, decay = TRUE, cores = 1){
-  classify1 <- function(x, tree, threshold = 0.9){
-    path <- integer(100)
-    Akws <- numeric(100)
-    scores = numeric(100) # log probs of seq given best model
-    cAkws <- numeric(100) # cumulative product of akaike weights
-    counter <- 1
-    seqhash <- paste(openssl::md5(as.vector(x)))
-    matches <- which(attr(tree, "hashes") == seqhash)
-    if(length(matches) == 0) matches <- NA
+classify <- function(x, tree, threshold = 0.9, decay = TRUE, cores = 1){
+  classify1 <- function(x, tree, threshold = 0.9, decay = TRUE){
+    path <- ""
+    akw <- 1
+    cakw <- 1
     res <- "" # lineage above the root node
-    reachedleaf <- TRUE
     while(is.list(tree)){
       no_mods <- length(tree)
       sc <- numeric(no_mods) # scores (log probabilities)
@@ -53,115 +47,53 @@ classify <- function(x, tree, threshold = 0.99, decay = TRUE, cores = 1){
       total_score <- aphid::logsum(sc)
       akwgts <- exp(sc - total_score)
       best_model <- which.max(akwgts)
-      path[counter] <- best_model
-      Akws[counter] <- akwgts[best_model]
-      cAkws[counter] <- akwgts[best_model] * if(counter == 1) 1 else cAkws[counter - 1]
-      scores[counter] <- sc[best_model]
-      #threshold_met <- akwgts[best_model] >= threshold
-      threshold_met <- (if(decay) cAkws[counter] else Akws[counter]) >= threshold
-      minscore_met <- sc[best_model] >= min(attr(tree[[best_model]], "scores"))
-      if(!(threshold_met)){# & minscore_met)){# | best_model == no_mods + 1){
-        #path <- path[1:counter]
-        # path vector should be 2 shorter than scores & weights
-        path <- if(counter < 3) integer(0) else path[1:(counter - 2)] ###TODO what if counter = 1?
-        Akws <- Akws[1:counter]
-        cAkws <- cAkws[1:counter]
-        scores <- scores[1:counter]
-        reachedleaf <- FALSE
-        break
-      }
-      res <- attr(tree, "lineage")
+      newakw <- akwgts[best_model]
+      newcakw <- newakw * cakw
+      threshold_met <- threshold <= if(decay) newcakw else newakw
+      minscore_met <- sc[best_model] >= attr(tree[[best_model]], "minscore")
+      if(!(threshold_met & minscore_met)) break
+      path <- paste0(path, best_model)
+      akw <- newakw
+      cakw <- newcakw
       tree <- tree[[best_model]]
-      counter <- counter + 1
+      res <- attr(tree, "lineage")
     }
-    # if(res[counter, 1] == 0) res <- res[1:(counter - 1), ]
-    # if(path[counter] == 0){# i.e. reached leaf node
-    if(reachedleaf){# i.e. reached leaf node
-      if(matches[1] %in% attr(tree, "sequences")){ # i.e. there is an exact match
-        res <- attr(tree, "lineage") # can confidently drop 1 level
-        # path <- path[1:(counter - 1)]
-        path <- if(counter < 2) integer(0) else path[1:(counter - 1)]
-        ## just a backup,  counter would therefore have to be > 1
-        ## unlesss tree is a single leaf
-      }else{ # reached leaf but no exact match
-        # path <- path[1:(counter - 1)]
-        path <- if(counter < 3) integer(0) else path[1:(counter - 2)]
-        reachedleaf <- FALSE
-      }
-      if(counter > 1){
-        Akws <- Akws[1:(counter - 1)]
-        cAkws <- cAkws[1:(counter - 1)]
-        scores <- scores[1:(counter - 1)]
-      }else{
-        Akws <- cAkws <- scores <- numeric(0)
-      }
-    }
-    attr(res, "path") <- path
-    attr(res, "scores") <- scores
-    attr(res, "weights") <- Akws
-    attr(res, "cweights") <- cAkws
-    attr(res, "threshold") <- threshold_met
-    attr(res, "minscore") <- minscore_met
-    attr(res, "matches") <- matches
-    attr(res, "reachedleaf") <- reachedleaf
+    score <- if(decay) cakw else akw
+    res <- paste(c(res, path, paste(score), paste(threshold_met),
+                   paste(minscore_met)), collapse = "%")
     return(res)
+  }
+  unpack <- function(v){# v is a vector of %-delimited strings
+    v <- strsplit(v, split = "%")
+    out <- sapply(v, function(s) s[1])
+    attr(out, "paths") <- sapply(v, function(s) s[2])
+    attr(out, "scores") <- as.numeric(sapply(v, function(s) s[3]))
+    attr(out, "threshold_met") <- as.logical(sapply(v, function(s) s[4]))
+    attr(out, "minscore_met") <- as.logical(sapply(v, function(s) s[5]))
+    return(out)
   }
   if(is.list(x)){
     if(inherits(cores, "cluster")){
-      res <- parallel::parLapply(cores, x, classify1, tree, threshold)
+      res <- parallel::parSapply(cores, x, classify1, tree, threshold, decay)
     }else if(cores == 1){
-      res <- lapply(x, classify1, tree, threshold)
+      res <- sapply(x, classify1, tree, threshold, decay)
     }else{
-      #nseq <- length(x)
       navailcores <- parallel::detectCores()
       if(identical(cores, "autodetect")) cores <- navailcores - 1
       if(!(mode(cores) %in% c("numeric", "integer"))) stop("Invalid 'cores' object")
       if(cores > navailcores) stop("Insufficient CPUs available")
       if(cores > 1){
         cl <- parallel::makeCluster(cores)
-        res <- parallel::parLapply(cl, x, classify1, tree, threshold)
+        res <- parallel::parSapply(cl, x, classify1, tree, threshold, decay)
         parallel::stopCluster(cl)
       }else{
-        res <- lapply(x, classify1, tree, threshold)
+        res <- sapply(x, classify1, tree, threshold, decay)
       }
     }
-    return(res)
+    res <- unpack(res)
   }else{
-    return(classify1(x, tree, threshold))
+    res <- unpack(classify1(x, tree, threshold, decay))
   }
+  return(res)
 }
-
-
-
-# res[counter, 1] <- best_model
-# res[counter, 2] <- akwgts[best_model]
-# res[counter, 3] <- sc[best_model]
-
-# seqs <- seqs[attr(tree, "sequences")]
-# # an integer vector of indices pointing to the DNAbin obj (saves on memory)
-# # attributes are stripped during subsetting
-# attr(seqs, "species") <- seqattr$species[attr(tree, "sequences")]
-# attr(seqs, "description") <- seqattr$description[attr(tree, "sequences")]
-# attr(seqs, "lineage") <- seqattr$lineage[attr(tree, "sequences")]
-# if(length(unique(attr(seqs, "lineage"))) == 1){
-#   lineage <- attr(seqs, "lineage")[1]
-# }else{
-#   lineages <- lapply(attr(seqs, "lineage"), function(s) strsplit(s, split = ";")[[1]])
-#   linlengths <- sapply(lineages, length)
-#   whichminlen <- which.min(linlengths)
-#   minlen <- linlengths[whichminlen]
-#   lineage <- ""
-#   for(i in 1:minlen){
-#     if(all(sapply(lineages, function(e) lineages[[whichminlen]][i] %in% e))){
-#       lineage <- paste0(lineage, lineages[[whichminlen]][i], ";")
-#     }
-#   }
-#   lineage <- gsub(";$", "\\.", lineage)
-# }
-# out <- list()
-# out$lineage <- lineage
-# out$sequences <- seqs
-# out$results <- res
-# out$threshold_met <- threshold_met
-# out$minscore_met <- minscore_met
-# class(out) <- "classification"
+################################################################################
