d4d05653387103e4b2f03c62f1642c36e61f32b1
shaunpwilkinson <shaunpwilkinson@gmail.com>
2018-03-12 12:35:47 +0900

    trim was not trimming, one job

--- R/trim.R
+++ R/trim.R
@@ -8,27 +8,28 @@
 #'   representing the primer sequences.
 #' @return a "DNAbin" object or a vector of concatenated character strings,
 #'   depending on the input.
-#' @details TBA
+#' @details Any sequences not containing the primer(s) in either direction are discarded.
+#'   Hence this function can also be used to de-multiplex sequences and remove indices,
+#'   though it will generally be faster to do this on the sequencing platform prior to
+#'   exporting the FASTQ files.
 #' @author Shaun Wilkinson
 #' @references TBA
 #' @examples ##TBA
 ################################################################################
 trim <- function(x, up, down = NULL){ # trims primers and/or indices
   isbin <- .isDNA(x)
-  if(isbin){
-    x <- dna2char(x)
-  }else{
-    if(mode(x) != "character") stop("Invalid input\n")
-  }
+  if(isbin) x <- dna2char(x) else stopifnot(mode(x) == "character")
   quals <- attr(x, "quality")
   hasq <- !is.null(quals)
   if(.isDNA(up)) up <- dna2char(up)
   if(.isDNA(down)) down <- dna2char(down)
+  ncup <- nchar(up) # store primer length prior to disambiguation
+  ncdn <- nchar(down) # returns empty integer if null
   up <- disambiguate(up)
   if(!is.null(down)) down <- disambiguate(rc(down))
   strev <- function(x) sapply(lapply(lapply(x, charToRaw), rev), rawToChar)
   isok <- function(z) z[1] > 0 & length(z) == 1
-  pattern <- paste0(".*", up, ".+", if(!is.null(down)) paste0(down, ".*") else NULL)
+  pattern <- paste0(up, ".+", down) ## down can be null
   gx1 <- gregexpr(pattern, x)
   hasp <- sapply(gx1, isok) # has primers?
   x[!hasp] <- rc(x[!hasp]) # complement rejects
@@ -39,7 +40,14 @@ trim <- function(x, up, down = NULL){ # trims primers and/or indices
   hasp[!hasp] <- tmp
   if(sum(hasp) == 0) return(if(isbin) raw(0) else character(0))
   starts <- unlist(gx1[hasp])
-  stops <- sapply(gx1[hasp], function(g) attr(g, "match.length"))
+  stops <- sapply(gx1[hasp], function(g) attr(g, "match.length")) + starts - 1
+  starts <- starts + ncup
+  if(!is.null(down)) stops <- stops - ncdn
+  discards <- stops - starts < 50
+  hasp[discards] <- FALSE
+  if(sum(hasp) == 0) return(if(isbin) raw(0) else character(0))
+  starts <- starts[!discards]
+  stops <- stops[!discards]
   res <- mapply(substr, x[hasp], starts, stops)
   if(hasq) quals <- unname(mapply(substr, quals[hasp], starts, stops))
   attr(res, "quality") <- quals
